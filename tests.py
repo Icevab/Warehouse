be1a9d24ad04da41e51a0de7b92bd663e1eeaa00
Icevab <dimasikooooy@gmail.com>
2023-01-06 18:54:33 +0300

    a little bit of refactoring and adding new methods

--- tests.py
+++ tests.py
@@ -1,14 +1,16 @@
 from main import *
 
 
+
 i1 = Item(name="apple", nodes=["food", "fruit"], price=5)
 i2 = Item(name="some vynil", nodes=["music", "vynils"], price=30)
-i3 = Item(name="keyboard", nodes=["electronics", "pc-related", "keyboards"], price=25)
+i3 = Item(name="keyboard", nodes=["electronics", "pc-related", "keyboards"], price=5)
 
 warehouse = Warehouse(
     name="Warehouse", location="Cmyui Avenue", items=[], cash=100)
-provider = Provider(name="Provider", location="Cmyui Street",
+provider = Provider(name="That one provider", location="Cmyui Street",
                     items=[i1, i2, i3], carriers=[], cash=10000)
+
 carrier1 = Carrier(name="cmyui", speed=2, desired_wage=1, cash=0)
 carrier2 = Carrier(name="cmyui2", speed=4, desired_wage=5, cash=0)
 carrier3 = Carrier(name="cmyui3", speed=1, desired_wage=1, cash=0)
@@ -25,51 +27,11 @@ provider.add_carrier(carrier4)
 provider.add_carrier(carrier5)
 provider.add_carrier(carrier6)
 
-# warehouse.buy(2, provider, 0)
-
-# build_nodes_of_item(i3)
-
-# c = best_carrier(provider.carriers, 40)
-# print(c)
-
-# print(len(warehouse.items))
-
-# print(calculate_percent_off(i3, 67))
-print(callable(calculate_price))
-
-# items = {
-#     "fruit": {
-#         "apple": {
-#             "item_name": "apple",
-#             "price": 5,
-#             "amount": 203
-#         },
-#         "pear": {
-#             "item_name": "pear",
-#             "price": 6,
-#             "amount": 145
-#         }
-#     },
-#     "vegetables": {
-#         "tomato": {
-#             "item_name": "tomato",
-#             "price": 7,
-#             "amount": 254
-#         },
-#         "potato": {
-#             "item_name": "potato",
-#             "price": 3,
-#             "amount": 24
-#         }
-#     }
-# }
-
-# i_test = {
-#     "item_name": "grapes",
-#     "price": 4,
-#     "amount": 203
-# }
+warehouse.buy(2, provider, 0)
 
 
+all_warehouses = [warehouse]
+all_providers = [provider]
+all_carriers = [carrier1, carrier2, carrier3, carrier4, carrier5, carrier6]
+all_items = [i1, i2, i3]
 
-# print(provider.items)
